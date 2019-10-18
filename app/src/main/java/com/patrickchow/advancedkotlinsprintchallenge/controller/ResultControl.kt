6b8afb18501bdb182bfd01f4efd416deb35795b9
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-18 13:35:43 -0400

    Added a layout for result controller and inflated it.

--- app/src/main/java/com/patrickchow/advancedkotlinsprintchallenge/controller/ResultControl.kt
+++ app/src/main/java/com/patrickchow/advancedkotlinsprintchallenge/controller/ResultControl.kt
@@ -3,11 +3,14 @@ package com.patrickchow.advancedkotlinsprintchallenge.controller
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.patrickchow.advancedkotlinsprintchallenge.R
 
 // Extends BaseControl which is a child of Controller
 class ResultControl : BaseControl(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = inflater.inflate(R.layout.controller_result, container, false)
+
+        return view
     }
 
 }
