6d660f903f366cbd8f0b82edcf16d33aad2d6407
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-18 13:02:02 -0400

    Added Result Controller

---
+++ app/src/main/java/com/patrickchow/advancedkotlinsprintchallenge/controller/ResultControl.kt
@@ -0,0 +1,13 @@
+package com.patrickchow.advancedkotlinsprintchallenge.controller
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// Extends BaseControl which is a child of Controller
+class ResultControl : BaseControl(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
