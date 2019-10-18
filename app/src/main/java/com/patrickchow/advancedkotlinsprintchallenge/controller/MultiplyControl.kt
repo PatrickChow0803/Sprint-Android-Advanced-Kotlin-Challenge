5257929705379a0864ae3c2207d2be797468047c
Patrick Chow <PatrickChow0803@gmail.com>
2019-10-18 14:44:07 -0400

    Added functionality to the multiply button.

--- app/src/main/java/com/patrickchow/advancedkotlinsprintchallenge/controller/MultiplyControl.kt
+++ app/src/main/java/com/patrickchow/advancedkotlinsprintchallenge/controller/MultiplyControl.kt
@@ -3,13 +3,26 @@ package com.patrickchow.advancedkotlinsprintchallenge.controller
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bluelinelabs.conductor.RouterTransaction
+import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler
 import com.patrickchow.advancedkotlinsprintchallenge.R
+import kotlinx.android.synthetic.main.controller_multiply.view.*
 
 
 // Extends BaseControl which is a child of Controller
 class MultiplyControl :BaseControl(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         val view = inflater.inflate(R.layout.controller_multiply, container, false)
+
+        view.btn_multiply.setOnClickListener {
+
+            //Makes it so that the view transitions to Result Control
+            router.pushController(RouterTransaction.with(ResultControl())
+                //Gives the transition a horizontal slide animation going from MultiplyControl to ResultControl
+                .pushChangeHandler(HorizontalChangeHandler())
+                //Gives the transition a horizontal slide when going backwards.
+                .popChangeHandler(HorizontalChangeHandler()))
+        }
 
         return view
     }
